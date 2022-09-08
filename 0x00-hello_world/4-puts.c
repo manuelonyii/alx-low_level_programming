2a98dcacc694851d0b6a21ca379fc878a3a96444
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-08 16:26:54 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - printing it
+ * main - Printing it
  * 
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building multilingual puzzle");
+	puts("\"Programming is like building a  multilingual puzzle");
 	return (0);
 }

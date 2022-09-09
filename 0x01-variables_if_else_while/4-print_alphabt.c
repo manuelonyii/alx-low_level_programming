e21bce07b218aa0afeb7ff189ec7d3e9c9a8c001
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-09 16:46:35 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Ommit some alphabet
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+
+	putcher('\n');
+
+	return (0);
+}

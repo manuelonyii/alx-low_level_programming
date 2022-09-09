1299746af36cb08bcffd867089b27e31d6b60ac6
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-09 16:15:28 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase and uppercase
+ *
+ * Return: Always (Successful)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	
+	for(c = 'a'; c <= 'z' c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}

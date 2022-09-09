ad9af4f77f4bdda78c6aafb9ee33f4da6ab87c61
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-08 21:57:56 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,7 @@
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
- * 	followed by a new line, to standard error
+ * main - Prints "and that piece of art is useful" 
  *
  * Return: Always 1
  */

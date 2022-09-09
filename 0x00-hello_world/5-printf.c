a61fe379b0b7c7b6f7b90fdb4fe776dbbd7602fc
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-08 17:14:59 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print doc
+ *
+ * Return: Always 0 (Process)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0)
+}

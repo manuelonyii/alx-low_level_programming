b94210ac526988b217561531c6effe97fbac2a2e
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-09 09:29:07 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	10 | int s = strlen("and that piece of art is useful" - Dora Korper, 2015-10-19\n");
+	 int s = strlen("and that piece of art is useful" Dora Korper, 2015 10 19\n");
 	write(2, "and that piece of art is useful" - Dora Korper, 2015-10-19\n", s);
 	return (1);
 }

9c023425b10e3785a9d738c82608825176b7e6cc
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-10 17:15:14 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,11 @@
 #include <unistd.h>
 /**
  * main - entry point
- * Return: 1
+ *
+ * Return: return 1
  */
 int main(void)
 {
-	 int s = strlen("and that piece of art is useful"); Dora Korper, 2015 10 19\n";
-	write(2, "and that piece of art is useful" - Dora Korper, 2015-10-19\n";
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

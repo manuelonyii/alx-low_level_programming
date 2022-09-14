7e8ab5f3446a39400951e8d00e13706292f3e877
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-13 19:20:00 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,8 +4,15 @@
 * @n: input
 * Return: Always 0 (Success)
 */
-voidprint_times_table(int n)
+void print_times_table(int n)
 {
+	print_times_table(3);
+	    _putchar('\n');
+	        print_times_table(5);
+		    _putchar('\n');
+		        print_times_table(98);
+			    _putchar('\n');
+			        print_times_table(12); 
 
 return (0);
 }

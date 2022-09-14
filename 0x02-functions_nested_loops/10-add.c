0327407e2c331c7698d1822917c0b9dd645e249c
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-13 18:36:32 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+Task10
+
+
+include"main.h"
+
+/**
+* add - add two integers
+* @d: two integer arguments
+* @b: two integer arguments
+* Return: sum
+*/
+
+int add(int b, int d)
+{
+
+int sum;
+
+sum = b + d;
+
+return (sum);
+}

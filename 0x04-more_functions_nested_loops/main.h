2420b7598f1125d1f1c877fb6aa1c887e13b1696
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-15 16:46:07 -0700

    Task 5

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,14 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numders(void);
+void print_numbers(void);
+void print_most_numbers(void);
 void print_more_numbers(void);
 void print_numbers(void);
 void print_line(int n);

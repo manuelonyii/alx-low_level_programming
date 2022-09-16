159c15bc17daab2564233b7eae8be2b73c535130
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-09-15 18:24:51 -0700

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+#include  <stdio.h>
+#include <math.h>
 
 /**
 *main - prints the largest prime factor of 612852475143
@@ -20,7 +21,7 @@ max = 2;
 n /= 2;
 }
 
-for (i = 3; i <= sqrt(n); i = i = 2)
+for (i = 3; i <= sqrt(n); i = i + 2)
 {
 while (n % i == 0)
 {

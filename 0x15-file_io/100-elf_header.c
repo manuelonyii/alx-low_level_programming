d4621da9079888be3938ab939a5ed71b1b3a5e10
manuelonyii <edvardjonas7@gnas7@gmail.com>
2022-10-31 18:58:41 -0700

    Task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -6,16 +6,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-voidcheck_elf(unsigned char *e_ident);
-voidprint_magic(unsigned char *e_ident);
-voidprint_class(unsigned char *e_ident);
-voidprint_data(unsigned char *e_ident);
-voidprint_version(unsigned char *e_ident);
-voidprint_abi(unsigned char *e_ident);
-voidprint_osabi(unsigned char *e_ident);
-voidprint_type(unsigned int e_type, unsigned char *e_ident);
-voidprint_entry(unsigned long int e_entry, unsigned char *e_ident);
-voidclose_elf(int elf);
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 /**
 * check_elf - Checks if a file is an ELF file.
